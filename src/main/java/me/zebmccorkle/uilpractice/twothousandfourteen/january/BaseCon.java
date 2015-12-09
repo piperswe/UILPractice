f0f071769b0b0661c0c82f9ef9a9e1ebd7b2375b
Zeb McCorkle <zmccorkle@classcoder.com>
2015-12-09 17:30:14 -0600

    BaseCon works.

--- src/main/java/me/zebmccorkle/uilpractice/twothousandfourteen/january/BaseCon.java
+++ src/main/java/me/zebmccorkle/uilpractice/twothousandfourteen/january/BaseCon.java
@@ -1,7 +1,34 @@
 package me.zebmccorkle.uilpractice.twothousandfourteen.january;
 
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by 19mccorkle.zebulon on 12/9/2015.
  */
 public class BaseCon {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("basecon.in"));
+
+        int amount = scanner.nextInt();
+        for (int i = 0; i < amount; i++) {
+            String type = scanner.next();
+            String val = scanner.next();
+            int intVal;
+            switch (type) {
+                case "H":
+                    intVal = Integer.parseInt(val, 16);
+                    break;
+                case "O":
+                    intVal = Integer.parseInt(val, 8);
+                    break;
+                case "B":
+                    intVal = Integer.parseInt(val, 2);
+                    break;
+                default:
+                    intVal = Integer.parseInt(val);
+            }
+            System.out.printf("%d %x %o %s%n", intVal, intVal, intVal, Integer.toBinaryString(intVal));
+        }
+    }
 }
